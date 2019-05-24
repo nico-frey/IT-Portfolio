6db0b23fe2cc78e73dc0c386e57f355c4e99ba89
nico-frey <nico.frey@gmx.ch>
2019-05-25 01:20:12 +0200

    Simple Javascript added and adjusted

---
+++ _scripts/ScrollReveal.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.SRheroMainHeading', { delay: 0, distance:'100px', duration: 1000, origin: 'left', easing: 'cubic-bezier(0,.38,.52,1.02)' });
+ScrollReveal().reveal('.SRheroSecHeading', { delay: 100, distance:'100px', duration: 1000, origin: 'left', easing: 'cubic-bezier(0,.38,.52,1.02)' });
+
+ScrollReveal().reveal('.imgHero', { delay: 0, distance:'50px', duration: 1000, origin: 'right', easing: 'cubic-bezier(0,0,.65,.99)' });
+
+ScrollReveal().reveal('.sct01', {  distance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('.sct02', { distance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('.sct03', { istance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('.sct04', { distance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
