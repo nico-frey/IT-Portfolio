b23ed59a8778b413a2288783e5a8fbe9c2f11019
nico-frey <nico.frey@gmx.ch>
2019-05-26 21:49:19 +0200

    final commit

---
+++ _scripts/MySlick.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.team-slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 5,
+        slidesToScroll: 2
+        
+    });
+  });
--- _scripts/ScrollReveal.js
+++ _scripts/ScrollReveal.js
@@ -1,9 +1,34 @@
+//index on page load fade in
+
 ScrollReveal().reveal('.SRheroMainHeading', { delay: 0, distance:'100px', duration: 1000, origin: 'left', easing: 'cubic-bezier(0,.38,.52,1.02)' });
 ScrollReveal().reveal('.SRheroSecHeading', { delay: 100, distance:'100px', duration: 1000, origin: 'left', easing: 'cubic-bezier(0,.38,.52,1.02)' });
+ScrollReveal().reveal('.SRheroLink', { delay: 300, distance:'100px', duration: 1000, origin: 'left', easing: 'cubic-bezier(0,.38,.52,1.02)' });
 
 ScrollReveal().reveal('.imgHero', { delay: 0, distance:'50px', duration: 1000, origin: 'right', easing: 'cubic-bezier(0,0,.65,.99)' });
 
-ScrollReveal().reveal('.sct01', {  distance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
-ScrollReveal().reveal('.sct02', { distance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
-ScrollReveal().reveal('.sct03', { istance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
-ScrollReveal().reveal('.sct04', { distance:'50px', duration: 1000, origin: 'bottom', easing: 'cubic-bezier(0,0,.65,.99)' });
+//news on page load fade in
+ScrollReveal().reveal('#NewsFadeOne', { delay: 0, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#NewsFadeTwo', { delay: 100, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#NewsFadeThree', { delay: 200, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' }); 
+
+//contact on page load fade in
+ScrollReveal().reveal('#ContactFadeOne', { delay: 0, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#ContactFadeTwo', { delay: 100, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#ContactFadeThree', { delay: 200, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#ContactFadeFour', { delay: 300, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#ContactFadeFive', { delay: 400, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+
+//faq page on page load fade in FaqFadeSix
+
+ScrollReveal().reveal('#FaqFadeOne', { delay: 100, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeTwo', { delay: 150, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeThree', { delay: 200, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeFour', { delay: 250, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeFive', { delay: 300, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeSix', { delay: 350, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeSeven', { delay: 400, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeEight', { delay: 450, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeNine', { delay: 500, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeTen', { delay: 550, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+ScrollReveal().reveal('#FaqFadeEleven', { delay: 600, distance:'20px', duration: 500, origin: 'left', easing: 'cubic-bezier(0,0,.65,.99)' });
+
